ce6538ad4c3d639a31acb738d871e7f254159e3a
unknown <xuehonghao@yeah.net>
2020-04-21 18:13:03 +0800

    封装axios

---
+++ src/utils/api/index.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+// 基地址
+const BaseURL = "http://localhost:8080/";
+
+const api = axios.create({
+  baseURL: BaseURL,
+});
+
+// Add a request interceptor  请求拦截器
+api.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor  响应拦截器
+api.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+export default api;
+export { BaseURL };
