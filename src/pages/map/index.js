e22a24a31ca8f7b9245e37dd78e321a6d7f5ba68
unknown <xuehonghao@yeah.net>
2020-05-05 12:06:13 +0800

    初始化地图

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -4,6 +4,8 @@ import { NavBar, Icon } from "antd-mobile";
 
 import "./index.scss";
 
+import { getCurrCity } from "../../utils/currentCity";
+
 class index extends Component {
   componentDidMount() {
     // 初始化地图
@@ -11,11 +13,28 @@ class index extends Component {
   }
 
   // 初始化地图
-  initMap = () => {
+  initMap = async () => {
     const { BMap } = window;
+    // 创建地图实例
     const map = new BMap.Map("container");
-    var point = new BMap.Point(116.404, 39.915);
-    map.centerAndZoom(point, 15);
+    // 获取定位城市
+    const { value, label } = await getCurrCity();
+    // 创建地址解析器实例
+    let myGeo = new BMap.Geocoder();
+    // 将地址解析结果显示在地图上，并调整地图视野
+    myGeo.getPoint(
+      null,
+      function (point) {
+        if (point) {
+          // 初始化地图，设置中心点坐标和地图级别
+          map.centerAndZoom(point, 11);
+          // 添加控件
+          map.addControl(new BMap.NavigationControl());
+          map.addControl(new BMap.ScaleControl());
+        }
+      },
+      label
+    );
   };
   render() {
     return (
